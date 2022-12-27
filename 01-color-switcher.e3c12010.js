906986a45781398dc9888e3f69bccfdc71d09538
Linsted <109988731+Linsted@users.noreply.github.com>
2022-12-27 20:40:15 +0000

    Deploying to gh-pages from @ Linsted/goit-js-hw-09@e807706bed5f5b6e217d635a503aa5acc2c562a4 🚀

---
+++ 01-color-switcher.e3c12010.js
@@ -0,0 +1,2 @@
+const t={buttonStart:document.querySelector("button[data-start]"),buttonStop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let o=null;function e(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.buttonStart.addEventListener("click",(function(){o=setInterval(e,1e3),t.buttonStart.disabled=!0})),t.buttonStop.addEventListener("click",(function(){t.body.style.backgroundColor="white",clearInterval(o),t.buttonStart.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.e3c12010.js.map
