e807706bed5f5b6e217d635a503aa5acc2c562a4
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-27 22:39:42 +0200

    Finished 1st task.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+    buttonStart: document.querySelector('button[data-start]'),
+    buttonStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+}
+
+const TIME_INTERVAL = 1000;
+let ourIntervalId = null;
+
+refs.buttonStart.addEventListener('click', changeBodycolor)
+refs.buttonStop.addEventListener('click', stopChangeColor)
+
+
+function changeBodycolor() { 
+    ourIntervalId = setInterval(randomColor, TIME_INTERVAL)
+    refs.buttonStart.disabled = true;
+};
+function randomColor() {
+    refs.body.style.backgroundColor = getRandomHexColor()
+    }
+
+function stopChangeColor() {
+    refs.body.style.backgroundColor = 'white'
+    clearInterval(ourIntervalId)
+    refs.buttonStart.disabled = false;
+}
