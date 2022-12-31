3252be21cc643e0b4437e05dcd53d98b1d35cca6
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-31 14:46:45 +0200

    Updated task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,29 +3,58 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
 const refs = {
-    datePicker: document.querySelector('#datetime-picker')
+  datePicker: document.querySelector('#datetime-picker'),
+  buttonStart: document.querySelector(`button[data-start]`),
+  spanSeconds: document.querySelector(`span[data-seconds]`),
 
 }
 
-let smt = 0;
+refs.buttonStart.disabled = true;
 
-console.log(1);
+
+console.log(4);
+
+
+
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-    onClose(selectedDates) {
-        return selectedDates[0].getTime()  
-        // console.log(smt);
-        
+  onClose(selectedDates) {
+      
+    if (selectedDates[0].getTime() < options.defaultDate.getTime())
+    { return alert("Please choose a date in the future") };
+      refs.buttonStart.disabled = false;   
   },
 };
 flatpickr(refs.datePicker, options)
 
-if (options.selectedDates) {alert(1)};
+
+
 
 
 
