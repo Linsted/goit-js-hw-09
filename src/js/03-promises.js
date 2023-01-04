55a951ea9618e67aac89fa7f7de66991cb3737c1
Ivan Malenko <ivan.malenko18@gmail.com>
2023-01-04 14:53:27 +0200

    fixed task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,38 +1,22 @@
 import Notiflix from 'notiflix';
 import "notiflix/dist/notiflix-3.2.5.min.css";  
 
+form = document.querySelector('.form');
 
-let firstDelay;
-let delayStep;
-let amount;
-let ourDelay;
-let ourDelayTimer;
-
-const refs = {
-  inputDelay: document.querySelector('[name="delay"]'),
-  inputStep: document.querySelector('[name="step"]'),
-  inputAmount: document.querySelector('[name="amount"]'),
-  button: document.querySelector('[type="submit"]'),
-  form: document.querySelector('.form'),
-}
-
-
-refs.form.addEventListener('submit', onSubmit)
+form.addEventListener('submit', onSubmit)
 
 
 function onSubmit(e) {
   e.preventDefault();
-  firstDelay = refs.inputDelay.value;
-  delayStep = refs.inputStep.value;
-  amount = refs.inputAmount.value;
-  ourDelayTimer = Number(firstDelay);
-  ourDelay = Number(firstDelay);
+  const {delay, step, amount} = e.currentTarget.elements
 
- 
+ let delayStep = step.value;
+ let amountOfProm = amount.value;
+ let ourDelay = Number(delay.value);
 
-  for (let i = 1; i <= amount; i++) {
-    
-    setTimeout(() => {
+
+for (let i = 1; i <= amountOfProm; i++) {
+
       
       createPromise(i, ourDelay)
       .then(({ position, delay }) => {
@@ -41,27 +25,24 @@ function onSubmit(e) {
     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   });
     ourDelay += Number(delayStep)
-    }, ourDelayTimer)
-    ourDelayTimer += Number(delayStep)
-    
   }
 
 
 }
 
-
 function createPromise(position, delay) {
 
-  
-
-
-  const shouldResolve = Math.random() > 0.3;
 
   const promise = new Promise((resolve, reject) => {
-      if (shouldResolve) {
+      const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+    if (shouldResolve) {
         resolve({ position, delay })
       } else {
-      reject({ position, delay })}
+      reject({ position, delay })
+    }
+    }, delay)
+    
   })
 
   return promise;
